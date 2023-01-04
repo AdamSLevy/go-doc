3947d1a3c1899103d1459ef15436d685c9155069
Adam S. Levy <adam@aslevy.com>
2023-01-03 16:48:46 -0900

    Fix module cache paths

--- internal/cache/module.go
+++ internal/cache/module.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"aslevy.com/go-doc/internal/dlog"
 	"aslevy.com/go-doc/internal/godoc"
 )
 
@@ -17,6 +18,11 @@ var (
 	Dir      string = getCacheDir()
 	Rebuild  bool
 	Disabled bool
+)
+
+const (
+	PathSeparator       = string(os.PathSeparator)
+	ImportPathSeparator = "/"
 )
 
 func getCacheDir() string {
@@ -30,35 +36,63 @@ func getCacheDir() string {
 }
 
 type Cache struct {
-	GoVersion string
-	Dir       string
-	Rebuild   bool
-	Disabled  bool
+	Dir string
+
+	GoVersion  string
+	GoModCache string
+
+	Rebuild  bool
+	Disabled bool
 }
 
 func New(goCmd string) *Cache {
-	goVer := goVersion(goCmd)
+	goVersion, goModCache := goInfo(goCmd)
+	disabled := Disabled ||
+		goVersion == "" || goModCache == "" ||
+		Dir == "-" || Dir == ""
 	return &Cache{
-		GoVersion: goVer,
-		Dir:       Dir,
-		Rebuild:   Rebuild,
-		Disabled:  Disabled || goVer == "" || Dir == "-" || Dir == "",
+		Dir:        Dir,
+		GoVersion:  goVersion,
+		GoModCache: goModCache,
+		Rebuild:    Rebuild,
+		Disabled:   disabled,
 	}
 }
-func goVersion(goCmd string) string {
-	stdout, _ := exec.Command(goCmd, "env", "GOVERSION").Output()
-	return string(bytes.TrimSpace(stdout))
+func goInfo(goCmd string) (goVersion, goModCache string) {
+	defer func() {
+	}()
+
+	args := []string{"env", "GOVERSION", "GOMODCACHE"}
+	stdout, err := exec.Command(goCmd, args...).Output()
+	if err != nil {
+		dlog.Printf("failed to run `%s %s`: %v", goCmd, strings.Join(args, " "), err)
+		return "", ""
+	}
+
+	stdout = bytes.TrimSpace(stdout)
+	values := bytes.SplitN(stdout, []byte{'\n'}, 3)
+	if len(values) != 2 {
+		dlog.Printf("failed to parse the output of `%s %s`: %q", goCmd, strings.Join(args, " "), string(stdout))
+		return "", ""
+	}
+	goVersion = string(bytes.TrimSpace(values[0]))
+	goModCache = string(bytes.TrimSpace(values[1]))
+	goModCache = filepath.Clean(goModCache)
+	dlog.Printf("cache: go info: GOVERSION=%q", goVersion)
+	dlog.Printf("cache: go info: GOMODCACHE=%q", goModCache)
+	return
 }
 
 type Module struct {
 	Root      godoc.Dir
 	Packages  []godoc.Dir
+	CachePath string
 	CreatedAt time.Time
 }
 
 func (c *Cache) NewModule(importPath, dir string) *Module {
-	if c.Disabled ||
-		!strings.Contains(filepath.Base(dir), "@") {
+	cachePath := c.moduleCachePath(importPath, dir)
+	if cachePath == "" {
 		return nil
 	}
 	return &Module{
@@ -66,6 +100,7 @@ func (c *Cache) NewModule(importPath, dir string) *Module {
 			ImportPath: importPath,
 			Dir:        dir,
 		},
+		CachePath: cachePath,
 		CreatedAt: time.Now(),
 	}
 }
@@ -87,25 +122,24 @@ func (m *Module) registerPackages(registerPackage func(importPath, dir string)) 
 }
 
 func (c *Cache) Save(mod *Module) error {
-	if c.Disabled || mod == nil || len(mod.Packages) == 0 {
+	if c.Disabled ||
+		mod == nil ||
+		mod.CachePath == "" ||
+		len(mod.Packages) == 0 {
 		return nil
 	}
-	dir, file := c.moduleCachePath(mod.Root.ImportPath, mod.Root.Dir)
-	if file == "" {
-		return fmt.Errorf("module is outside of go mod cache")
-	}
-	if err := os.MkdirAll(dir, 0755); err != nil {
+
+	if err := os.MkdirAll(filepath.Dir(mod.CachePath), 0755); err != nil {
 		return fmt.Errorf("failed to create cache dir: %w", err)
 	}
 
-	f, err := os.Create(filepath.Join(dir, file))
+	f, err := os.Create(mod.CachePath)
 	if err != nil {
 		return fmt.Errorf("failed to create module cache file: %w", err)
 	}
 	defer f.Close()
 
-	e := json.NewEncoder(f)
-	if err := e.Encode(mod); err != nil {
+	if err := json.NewEncoder(f).Encode(mod); err != nil {
 		return fmt.Errorf("failed to encode module to cache file: %w", err)
 	}
 
@@ -119,12 +153,12 @@ func (c *Cache) Load(importPath, moduleDir string, register RegisterPackage) err
 		return fmt.Errorf("cache disabled")
 	}
 
-	dir, file := c.moduleCachePath(importPath, moduleDir)
-	if file == "" {
+	cachePath := c.moduleCachePath(importPath, moduleDir)
+	if cachePath == "" {
 		return fmt.Errorf("module is outside of go mod cache")
 	}
 
-	f, err := os.Open(filepath.Join(dir, file))
+	f, err := os.Open(cachePath)
 	if err != nil {
 		return fmt.Errorf("failed to open module cache file: %w", err)
 	}
@@ -150,26 +184,39 @@ const (
 	moduleCacheStdlib = "stdlib@"
 )
 
-func (c *Cache) moduleCachePath(importPath, modDir string) (dir, filename string) {
+func (c *Cache) moduleCachePath(importPath, modDir string) string {
+	if c.Disabled {
+		return ""
+	}
+
+	if isStdlib(importPath) {
+		return filepath.Join(c.Dir, "go-stdlib",
+			// importPath is empty or "cmd", so this will either be
+			// "src" or "src/cmd"
+			"src", importPath) +
+			"@" + c.GoVersion + moduleCacheName
+	}
+
+	if !c.isInGoModCache(modDir) || !hasVersionSuffix(modDir) {
+		return ""
+	}
+
+	// relModDir is the module directory relative to the go mod cache. We
+	// want to use the same directory structure as the go mod cache because
+	// the file names it uses are robust to case-insensitive file systems.
+	relModDir := strings.TrimPrefix(modDir, c.GoModCache)
+	return filepath.Join(c.Dir, relModDir) + moduleCacheName
+}
+func isStdlib(importPath string) bool {
 	switch importPath {
 	case "", "cmd":
-		return c.Dir, moduleCacheStdlib + c.GoVersion + moduleCacheName
+		return true
 	}
-	numImportSegments := strings.Count(importPath, "/")
-	numDirSegments := strings.Count(modDir, string(os.PathSeparator))
-	if numImportSegments > numDirSegments {
-		return "", ""
-	}
-	slash := len(modDir)
-	var lastSlash int
-	for i := 0; i < numImportSegments; i++ {
-		slash = strings.LastIndex(modDir[:slash], string(os.PathSeparator))
-		if lastSlash == 0 {
-			lastSlash = slash
-		}
-	}
-	rel := modDir[slash+1 : lastSlash+1]
-	dir = filepath.Join(c.Dir, rel)
-	filename = modDir[lastSlash+1:] + moduleCacheName
-	return dir, filename
+	return false
+}
+func (c *Cache) isInGoModCache(dir string) bool {
+	return strings.HasPrefix(dir, c.GoModCache+PathSeparator)
+}
+func hasVersionSuffix(dir string) bool {
+	return strings.Contains(filepath.Base(dir), "@")
 }
